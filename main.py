cf5cc738d3c750556a156b1cbf88f6a99acb0ba4
Adela Tatum <80791686+addy-luna@users.noreply.github.com>
2021-08-19 17:18:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+secret_word = 'apple'
+guess = ''
+guess_count = 0
+guess_limit = 3
+out_of_guess = False
+
+while guess != secret_word and not(out_of_guess):
+  if guess_count < guess_limit:
+    guess = input('enter guess: ')
+    guess_count += 1
+  else:
+    out_of_guess = True
+
+if out_of_guess:
+  print('you lost')
+else:
+  print('You win!')
